66e7c4c8dd3931fa79a8de5ae26eae4cd20dc715
aymantabissi <aymanraisse7@email.com>
2025-02-02 14:48:28 +0100

    creation button delete avec function

--- frontend/src/componentes/ProductCard.jsx
+++ frontend/src/componentes/ProductCard.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { useProductStore } from '../store/product';
 
 function ProductCard({ product }) {
+    const {deleteProduct}=useProductStore()
+    const handleDeleteProduct = async (pid) => {
+        try {
+            const { success, message } = await deleteProduct(pid);
+            
+            if (success) {
+                alert("Product deleted successfully");
+            } else {
+                alert(`Failed to delete product: ${message}`);
+            }
+        } catch (error) {
+            console.error("Error deleting product:", error);
+            alert("An error occurred while deleting the product");
+        }
+    };
+
   return (
     <div className="bg-white rounded-xl shadow-md overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg">
       <img 
@@ -19,6 +36,7 @@ function ProductCard({ product }) {
           </button>
           <button 
             className="bg-red-600 text-white px-5 py-2 rounded-lg hover:bg-red-700 transition focus:outline-none focus:ring-2 focus:ring-red-500"
+            onClick={()=>handleDeleteProduct(product._id)}
           >
             Delete
           </button>
